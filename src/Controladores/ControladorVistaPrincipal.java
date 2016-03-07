e1005e524b4dbc981e73ab6a3a41bdbd266e451c
AlexMtz <yamop.16@gmail.com>
2016-03-07 14:16:08 -0600

    se agrego código al controlador de la interfaz principal
    Se agrego código para controlar los componentes de la interfaz gráfica
    principal

--- src/Controladores/ControladorVistaPrincipal.java
+++ src/Controladores/ControladorVistaPrincipal.java
@@ -5,10 +5,51 @@
  */
 package Controladores;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+
 /**
  *
  * @author ALEX
  */
-public class ControladorVistaPrincipal {
+public class ControladorVistaPrincipal implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        JButton botonClic = (JButton) ae.getSource();
+        
+        if(botonClic.getText().equalsIgnoreCase("Algoritmo Extendido de Euler")){
+            
+        }
+        
+        if(botonClic.getText().equalsIgnoreCase("Congruencia")){
+            
+        }
+        
+        if(botonClic.getText().equalsIgnoreCase("Criba de Erastotenes")){
+            
+        }
+        
+        if(botonClic.getText().equalsIgnoreCase("Entropia de Shannon")){
+            
+        }
+        
+        if(botonClic.getText().equalsIgnoreCase("Exponenciacion Modular")){
+            
+        }
+        
+        if(botonClic.getText().equalsIgnoreCase("Minimo Comun Divisor (M.C.D.)")){
+            
+        }
+        
+        if(botonClic.getText().equalsIgnoreCase("acerca de...")){
+            
+        }
+        
+        if(botonClic.getText().equalsIgnoreCase("salir")){
+            System.exit(0);
+        }
+    }
     
 }
