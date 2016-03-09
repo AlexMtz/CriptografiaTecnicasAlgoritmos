f533b612e6a6d25ff686243dc144fe5a98e64d81
AlexMtz <yamop.16@gmail.com>
2016-03-09 10:50:42 -0600

    Se agrego la interfaz de la Criba de Erastotenes
    Se garegaron los elementos de la interfaz de la criba de Erastotenes,
    así como el metodo que inicializa la fuente y estilo de cada uno de los
    elementos de la interfaz.

--- src/Vistas/VistaCriba.java
+++ src/Vistas/VistaCriba.java
@@ -5,6 +5,8 @@
  */
 package Vistas;
 
+import Recursos.fuentes.FontBSD;
+
 /**
  *
  * @author ALEX
@@ -16,6 +18,8 @@ public class VistaCriba extends javax.swing.JFrame {
      */
     public VistaCriba() {
         initComponents();
+        this.setLocationRelativeTo(null);
+        setFonts();
     }
 
     /**
@@ -27,17 +31,133 @@ public class VistaCriba extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        lb_tituloCriba = new javax.swing.JLabel();
+        lb_numeroCriba = new javax.swing.JLabel();
+        txt_numeroCriba = new javax.swing.JTextField();
+        btn_cribarCriba = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        txt_solucionCriba = new javax.swing.JTextArea();
+        lb_infoCriba = new javax.swing.JLabel();
+        btn_ayudaCriba = new javax.swing.JButton();
+        btn_limpiarCriba = new javax.swing.JButton();
+        btn_regresarCriba = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(52, 152, 219));
+
+        lb_tituloCriba.setFont(new java.awt.Font("BSD I Believe In Love", 1, 48)); // NOI18N
+        lb_tituloCriba.setForeground(new java.awt.Color(240, 240, 240));
+        lb_tituloCriba.setText("Criba de Erastotenes");
+
+        lb_numeroCriba.setFont(new java.awt.Font("BSD I Believe In Love", 1, 18)); // NOI18N
+        lb_numeroCriba.setForeground(new java.awt.Color(240, 240, 240));
+        lb_numeroCriba.setText("numero de elemeentos a cribar:");
+
+        txt_numeroCriba.setFont(new java.awt.Font("BSD I Believe In Love", 1, 18)); // NOI18N
+        txt_numeroCriba.setText("###");
+
+        btn_cribarCriba.setBackground(new java.awt.Color(243, 156, 18));
+        btn_cribarCriba.setFont(new java.awt.Font("BSD I Believe In Love", 1, 18)); // NOI18N
+        btn_cribarCriba.setForeground(new java.awt.Color(240, 240, 240));
+        btn_cribarCriba.setText("cribar");
+
+        txt_solucionCriba.setColumns(20);
+        txt_solucionCriba.setFont(new java.awt.Font("BSD I Believe In Love", 1, 18)); // NOI18N
+        txt_solucionCriba.setRows(5);
+        jScrollPane1.setViewportView(txt_solucionCriba);
+
+        lb_infoCriba.setFont(new java.awt.Font("BSD I Believe In Love", 1, 18)); // NOI18N
+        lb_infoCriba.setForeground(new java.awt.Color(240, 240, 240));
+        lb_infoCriba.setText("Info:");
+
+        btn_ayudaCriba.setBackground(new java.awt.Color(243, 156, 18));
+        btn_ayudaCriba.setFont(new java.awt.Font("BSD I Believe In Love", 1, 18)); // NOI18N
+        btn_ayudaCriba.setForeground(new java.awt.Color(240, 240, 240));
+        btn_ayudaCriba.setText("ayuda");
+
+        btn_limpiarCriba.setBackground(new java.awt.Color(243, 156, 18));
+        btn_limpiarCriba.setFont(new java.awt.Font("BSD I Believe In Love", 1, 18)); // NOI18N
+        btn_limpiarCriba.setForeground(new java.awt.Color(240, 240, 240));
+        btn_limpiarCriba.setText("limpiar campos");
+
+        btn_regresarCriba.setBackground(new java.awt.Color(243, 156, 18));
+        btn_regresarCriba.setFont(new java.awt.Font("BSD I Believe In Love", 1, 18)); // NOI18N
+        btn_regresarCriba.setForeground(new java.awt.Color(240, 240, 240));
+        btn_regresarCriba.setText("regresar");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(231, 231, 231)
+                        .addComponent(lb_tituloCriba))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(254, 254, 254)
+                        .addComponent(lb_numeroCriba)
+                        .addGap(18, 18, 18)
+                        .addComponent(txt_numeroCriba, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(451, 451, 451)
+                        .addComponent(btn_cribarCriba))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(lb_infoCriba)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(0, 101, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 801, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(99, 99, 99))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(btn_regresarCriba)
+                        .addGap(18, 18, 18)
+                        .addComponent(btn_limpiarCriba)
+                        .addGap(18, 18, 18)
+                        .addComponent(btn_ayudaCriba)
+                        .addGap(58, 58, 58))))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(lb_tituloCriba)
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lb_numeroCriba)
+                    .addComponent(txt_numeroCriba, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(btn_cribarCriba)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 303, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btn_ayudaCriba)
+                    .addComponent(btn_limpiarCriba)
+                    .addComponent(btn_regresarCriba))
+                .addGap(13, 13, 13)
+                .addComponent(lb_infoCriba)
+                .addContainerGap())
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
@@ -79,5 +199,29 @@ public class VistaCriba extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btn_ayudaCriba;
+    private javax.swing.JButton btn_cribarCriba;
+    private javax.swing.JButton btn_limpiarCriba;
+    private javax.swing.JButton btn_regresarCriba;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel lb_infoCriba;
+    private javax.swing.JLabel lb_numeroCriba;
+    private javax.swing.JLabel lb_tituloCriba;
+    private javax.swing.JTextField txt_numeroCriba;
+    private javax.swing.JTextArea txt_solucionCriba;
     // End of variables declaration//GEN-END:variables
+
+    private void setFonts() {
+        FontBSD fonts = new FontBSD();
+        lb_tituloCriba.setFont(fonts.getMyFont(1, 48));
+        lb_numeroCriba.setFont(fonts.getMyFont(1, 18));
+        lb_infoCriba.setFont(fonts.getMyFont(1, 14));
+        txt_numeroCriba.setFont(fonts.getMyFont(1, 18));
+        txt_solucionCriba.setFont(fonts.getMyFont(1, 18));
+        btn_cribarCriba.setFont(fonts.getMyFont(1, 18));
+        btn_regresarCriba.setFont(fonts.getMyFont(1, 18));
+        btn_limpiarCriba.setFont(fonts.getMyFont(1, 18));
+        btn_ayudaCriba.setFont(fonts.getMyFont(1, 18));
+    }
 }
